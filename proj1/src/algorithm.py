3e59de54ad45eeb7ab802434e8d49c20796fa2fa
Rui Soares <rui.pedro.csoares@gmail.com>
2018-10-23 16:01:16 +0100

    points

--- proj1/src/algorithm.py
+++ proj1/src/algorithm.py
@@ -13,3 +13,34 @@ def ConvertToBinary(img):
     _,binary = threshold(y,127,255,THRESH_BINARY_INV + THRESH_OTSU)
     return binary
 
+def SweepLeftRight(img):
+    height, width = img.shape[0], img.shape[1]
+    for y in range(0,height):
+        for x in range(0,width):
+            if img[y][x] == 255:
+                break
+    return x,y
+
+def SweepRightLeft(img):
+    height, width = img.shape[0], img.shape[1]
+    for y in range(0,height):
+        for x in range(width-1,0,-1):
+            if img[y][x] == 255:
+                break
+    return x,y
+    
+def SweepTopBottom(img):
+    height, width = img.shape[0], img.shape[1]
+    for x in range(0,width):
+        for y in range(0,height):
+            if img[y][x] == 255:
+                break
+    return x,y
+
+def SweepBottomTop(img):
+    height, width = img.shape[0], img.shape[1]
+    for x in range(0,width):
+        for y in range(height-1,0,-1):
+            if img[y][x] == 255:
+                break
+    return x,y
--- proj1/src/main.py
+++ proj1/src/main.py
@@ -5,12 +5,20 @@ from cv2 import *
 
 #img = camera.CaptureCameraImage()
 
-img = imread('../imgs/allright.png')
+img = imread('../imgs/two.png')
 img = algorithm.SmoothImage(img)
 img = algorithm.ConvertToYCbCr(img)
 img = algorithm.ConvertToBinary(img)
+x1,y1 = algorithm.SweepLeftRight(img)
+x2,y2 = algorithm.SweepRightLeft(img)
+x3,y3 = algorithm.SweepTopBottom(img)
+x4,y4 = algorithm.SweepBottomTop(img)
+print(x1,y1)
+print(x2,y2)
+print(x3,y3)
+print(x4,y4)  
 
 namedWindow("Binary")
 imshow("Binary",img)
-waitKey(5000)
+waitKey(0)
 #gui.StartGUI()
