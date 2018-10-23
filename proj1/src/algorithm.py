7d1efdf5b3e2e5062654ca088791729da8586af1
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-10-23 15:14:56 +0100

    binary file is working

--- proj1/src/algorithm.py
+++ proj1/src/algorithm.py
@@ -3,3 +3,13 @@ from cv2 import *
 def SmoothImage(img):
     blur = bilateralFilter(img,5,75,75)
     return blur
+
+def ConvertToYCbCr(img):
+    ycbcr = cvtColor(img, COLOR_BGR2YCR_CB)
+    return ycbcr
+
+def ConvertToBinary(img):
+    y,cb,cr = split(img)
+    _,binary = threshold(y,127,255,THRESH_BINARY_INV + THRESH_OTSU)
+    return binary
+
--- proj1/src/main.py
+++ proj1/src/main.py
@@ -3,10 +3,15 @@ import camera
 import gui
 from cv2 import *
 
-img = camera.CaptureCameraImage()
-img = algorithm.SmoothImage(img)
+#img = camera.CaptureCameraImage()
 
-namedWindow("xd")
-imshow("xd",img)
+img = imread('../imgs/one.jpg')
+img = algorithm.Gaussian(img)
+img = algorithm.SmoothImage(img)
+img = algorithm.ConvertToYCbCr(img)
+img = algorithm.ConvertToBinary(img)
+
+namedWindow("Binary")
+imshow("Binary",img)
 waitKey(5000)
 #gui.StartGUI()
