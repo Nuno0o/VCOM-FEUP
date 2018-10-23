7283435ee66c9c183f07a5f8c19a7145cf45cfd1
Nuno <up201405079@fe.up.pt>
2018-10-23 16:32:40 +0100

    rectangle done

--- proj1/src/algorithm.py
+++ proj1/src/algorithm.py
@@ -1,4 +1,5 @@
 from cv2 import *
+import numpy
 
 def SmoothImage(img):
     blur = bilateralFilter(img,5,75,75)
@@ -13,34 +14,53 @@ def ConvertToBinary(img):
     _,binary = threshold(y,127,255,THRESH_BINARY_INV + THRESH_OTSU)
     return binary
 
-def SweepLeftRight(img):
+def SweepTopBottom(img):
     height, width = img.shape[0], img.shape[1]
     for y in range(0,height):
         for x in range(0,width):
             if img[y][x] == 255:
-                break
-    return x,y
+                return x,y
+    return -1,-1
 
-def SweepRightLeft(img):
+def SweepBottomTop(img):
     height, width = img.shape[0], img.shape[1]
-    for y in range(0,height):
-        for x in range(width-1,0,-1):
+    for y in range(height-1,-1,-1):
+        for x in range(0,width):
             if img[y][x] == 255:
-                break
-    return x,y
-    
-def SweepTopBottom(img):
+                return x,y
+    return -1,-1
+
+def SweepLeftRight(img):
     height, width = img.shape[0], img.shape[1]
     for x in range(0,width):
         for y in range(0,height):
             if img[y][x] == 255:
-                break
-    return x,y
+                return x,y
+    return -1,-1
 
-def SweepBottomTop(img):
+def SweepRightLeft(img):
     height, width = img.shape[0], img.shape[1]
-    for x in range(0,width):
-        for y in range(height-1,0,-1):
+    for x in range(width-1,-1,-1):
+        for y in range(0,height):
             if img[y][x] == 255:
-                break
-    return x,y
+                return x,y
+    return -1,-1
+
+def GetRectEdges(img):
+    x1,_ = SweepLeftRight(img)
+    x2,_ = SweepRightLeft(img)
+    _,y3 = SweepTopBottom(img)
+    _,y4 = SweepBottomTop(img)
+    topleft_x = x1
+    topleft_y = y3
+    bottomright_x = x2
+    bottomright_y = y4
+    return topleft_x, topleft_y, bottomright_x, bottomright_y
+
+def GetRectSection(img,(x1,y1),(x2,y2)):
+    #rectangle(img, (x1,y1), (x2,y2), 128, 1)
+    clone = img.copy()
+    roi = clone[y1:y2,x1:x2]
+    imshow("XDDD", roi)
+
+
--- proj1/src/gui.py
+++ proj1/src/gui.py
@@ -1,7 +1,7 @@
-from tkinter import *
+'''from tkinter import *
 
 def StartGUI():
     root = Tk()
     root.title("VCOM-2018")
     root.minsize(width=200,height=200)
-    root.mainloop()
+    root.mainloop()'''
